ba9abcce784a5c467a9d78bfe00591e0f8d66ae5
svntax <endless.answers@gmail.com>
2022-11-12 01:32:36 -0500

    Player now automoves. Added multi-jump support.

--- MetaBoy/MetaBoy.gd
+++ MetaBoy/MetaBoy.gd
@@ -13,10 +13,13 @@ onready var input_controls
 
 const SNAP = Vector2(0, 12)
 const MAX_FALL_SPEED = 248
+
 onready var gravity = 16
 onready var speed = 164
 onready var jump_speed = 320
-onready var jump_grace_timer = $JumpGraceTimer
+onready var double_jump_speed = 292
+onready var max_jumps = 2
+onready var jump_count = 0
 
 onready var velocity = Vector2()
 onready var face_direction = 1
@@ -25,25 +28,29 @@ onready var state_machine = $StateMachine
 onready var animation_player = $AnimationPlayer
 onready var action_player = $ActionPlayer
 onready var body = $MainBody
+onready var jump_grace_timer = $JumpGraceTimer
 onready var attack_cooldown_timer = $AttackCooldownTimer
 
 func _ready():
 	input_controls = KEYBOARD_CONTROLS
 
 func _physics_process(_delta):
-	velocity.x = 0
+	velocity.x = speed * face_direction
 	
 	# Gravity
 	velocity.y += get_gravity()
 	
 	# Movement
 	if can_move():
-		if Input.is_action_pressed(input_controls["left"]):
-			velocity.x -= get_speed()
-		if Input.is_action_pressed(input_controls["right"]):
-			velocity.x += get_speed()
-		if velocity.x != 0:
-			face_direction = sign(velocity.x)
+		# TODO: do we want the player to be able to turn at any time?
+		if Input.is_action_just_pressed(input_controls["left"]):
+			#velocity.x -= get_speed()
+			face_direction = -1
+		if Input.is_action_just_pressed(input_controls["right"]):
+			#velocity.x += get_speed()
+			face_direction = 1
+#		if velocity.x != 0:
+#			face_direction = sign(velocity.x)
 		body.scale.x = face_direction
 		if Input.is_action_just_pressed(input_controls["action"]):
 			_handle_action()
@@ -57,6 +64,17 @@ func _physics_process(_delta):
 		velocity.y = move_and_slide(velocity, Vector2.UP, true).y
 	else:
 		velocity.y = move_and_slide_with_snap(velocity, SNAP, Vector2.UP, true).y
+	
+	# Flip directions on horizontal collision
+	var slide_count = get_slide_count()
+	for i in range(slide_count):
+		var slide_collision = get_slide_collision(i)
+		var normal = slide_collision.normal
+		if normal.x != 0:
+			face_direction = sign(velocity.bounce(normal).x)
+	
+	if is_on_floor() and jump_count != 0:
+		jump_count = 0
 	
 	# Limit fall speed
 	if velocity.y > MAX_FALL_SPEED:
@@ -77,10 +95,17 @@ func can_move() -> bool:
 	return state_machine.is_in_movement_state()
 
 func can_jump() -> bool:
-	return can_move() and (is_on_floor() or is_jump_grace_active())
+	if jump_count == 0:
+		return can_move() and (is_on_floor() or is_jump_grace_active())
+	else:
+		return can_move() and jump_count < max_jumps
 
 func jump() -> void:
-	velocity.y = -get_jump_speed()
+	jump_count += 1
+	if jump_count > 1:
+		velocity.y = -get_double_jump_speed()
+	else:
+		velocity.y = -get_jump_speed()
 	state_machine.set_state(state_machine.States.JUMP)
 
 func get_gravity() -> float:
@@ -100,6 +125,9 @@ func get_speed() -> float:
 
 func get_jump_speed() -> float:
 	return jump_speed
+
+func get_double_jump_speed() -> float:
+	return double_jump_speed
 
 func is_on_ground() -> bool:
 	return is_on_floor()

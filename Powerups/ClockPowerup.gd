7189774a4f2a330ad39cc511ba2b99a57369db66
svntax <endless.answers@gmail.com>
2022-11-13 22:25:03 -0500

    Added sounds for clock and heart pickups.

--- Powerups/ClockPowerup.gd
+++ Powerups/ClockPowerup.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 onready var animation_player = $AnimationPlayer
 onready var time_text_root = $TimeTextRoot
+onready var pickup_sound = $PickupSound
 
 onready var player = get_tree().get_nodes_in_group("Players")[0]
 onready var time_bonus = 5
@@ -17,7 +18,10 @@ func _on_body_entered(body):
 		player.add_time(time_bonus)
 		animation_player.play("collect")
 		time_text_root.show()
+		pickup_sound.play()
 		was_collected = true
+		collision_layer = 0
+		collision_mask = 0
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	if anim_name == "collect":
--- Powerups/HeartPowerup.gd
+++ Powerups/HeartPowerup.gd
@@ -1,15 +1,28 @@
 extends Area2D
 
 onready var animation_player = $AnimationPlayer
+onready var sprite = $Sprite
+onready var pickup_sound = $PickupSound
 
 onready var player = get_tree().get_nodes_in_group("Players")[0]
-onready var hp_amount = 1
+onready var heal_amount = 1
+onready var picked_up = false
 
 func _ready():
 	animation_player.play("hover")
 	connect("body_entered", self, "_on_body_entered")
 
 func _on_body_entered(body):
+	if picked_up:
+		return
+	
 	if body == player:
-		player.heal(hp_amount)
-		queue_free()
+		picked_up = true
+		collision_layer = 0
+		collision_mask = 0
+		player.heal(heal_amount)
+		pickup_sound.play()
+		sprite.hide()
+
+func _on_PickupSound_finished():
+	queue_free()
